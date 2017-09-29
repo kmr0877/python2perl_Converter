032813a4d4f59ce50eb3a7d085cec0859baad4d7
Mohan <kmr0877@gmail.com>
2017-09-29 13:42:49 +1000

    handling with arguments done

--- pypl.pl
+++ pypl.pl
@@ -1,30 +1,10 @@
 #!/usr/bin/perl -w
-
-
-while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {
-
-        # translate #! line
-
-        print "#!/usr/bin/perl -w\n";
-
-    } elsif ($line =~ /^\s*(#|$)/) {
-
-        # Blank & comment lines can be passed unchanged
-
-        print $line;
-
-    } elsif ($line =~ /^\s*print\("(.*)"\)$/) {
-
-        # Python's print outputs a new-line character by default
-        # so we need to add it explicitly to the Perl print statement
-
-        print "print \"$1\\n\";\n";
-
-    } else {
-
-        # Lines we can't translate are turned into comments
-
-        print "#$line\n";
-    }
+$num_args = $#ARGV + 1;
+if ($num_args > 1) {
+    print "\nToo many arguments.";
+    exit;
 }
+elsif($num_args < 1){
+	print "\nToo few arguments.";
+}
+$python_script = $ARGV[0];
