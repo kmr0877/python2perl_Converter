cb659adcdd622d0ee451d23c2210cd50afa963c3
Mohan <kmr0877@gmail.com>
2017-09-30 12:59:14 +1000

    updated with variables

--- pypl.pl
+++ pypl.pl
@@ -5,14 +5,30 @@ if ($num_args > 1) {
     exit;
 }
 elsif($num_args < 1){
-	print "\nToo few arguments.";
+    print "\nToo few arguments.";
 }
 $python_script = $ARGV[0];
+
+open(my $fh, '<:encoding(UTF-8)', $python_script)
+  or die "Could not open file '$python_script' $!";
 sub in {
-	foreach $element (@_[1..$#_]){
-		if($_[0] eq $element){
-			return '';
-		}
-	}
-	return 1;
+    foreach $element (@_[1..$#_]){
+        if($_[0] eq $element){
+            return '';
+        }
+    }
+    return 1;
+}
+
+
+sub get_variables{
+    $i = 0;
+    foreach $row (@lines){
+        if($row =~ m/([\t]*\w[\w0-9_]*)[ ]*=/){
+            if(in $1,@vars){
+                $vars[$i] = $1;
+                $i ++;
+            }
+        }
+    }
 }
