1f2ffc600a19524c59cbc3839a3407f64f327a13
Mohan <kmr0877@gmail.com>
2017-10-02 13:01:26 +1100

    matching row by row

--- pypl.pl
+++ pypl.pl
@@ -40,3 +40,25 @@ sub end_lines{
         }
     }
 }
+sub translate_code{
+    foreach $row (@lines){
+        if(!(substr($row,0,1) eq "#")){
+            foreach $var (@vars){
+                if(!($row =~ m/\"[\w\W]*$var[\w\W]*\"/) and !($row =~ m/[\w]+$var[\w\W]*/) and !($row =~ m/[\w\W]*$var[\w]+/)){
+                    $row =~ s/$var/\$$var/g;
+                }
+            }
+            if($row =~ m/break/ and !($row =~ m/\"[\w\W]*break[\w\W]*\"/)){
+                $row =~ s/break/last/;
+            }
+            if($row =~ m/^import[ ]*[\w]*/ or $row =~ m/from[ ]*[\w]*[ ]*import/){
+                $row = "";
+            }
+            if(($row =~ m/print\(/ or $row =~ m/sys.stdout.write/)){
+                $row =~ s/sys.stdout.write\(([\w\W]*)\)/print $1/;
+                $row =~ s/print[ ]*\(([\w\W]*)\)/print $1,"\\n";/;
+            }
+        }
+    }
+
+}
